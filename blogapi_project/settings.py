0c0962a40a285f5ddbb4c0671df1e454cfcc5ceb
alindafortunate <alindafortunate5@gmail.com>
2024-06-26 16:50:08 +0300

    Added Permissions

--- blogapi_project/settings.py
+++ blogapi_project/settings.py
@@ -51,8 +51,8 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.AllowAny",
-    ]
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
 
 MIDDLEWARE = [
--- posts/views.py
+++ posts/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 from rest_framework import generics
 
 from .models import *
+from .permissions import IsAuthorOrReadOnly
 from .serializer import *
 
 
 # Create your views here.
 class PostList(generics.ListCreateAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Post.objects.all()
     serializer_class = PostSerializer
